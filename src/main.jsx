35be8b9961c5eb5a2cd044f85d9f8edb1f8b2720
Suryansh Pathak <pathaksuryansh156@gmail.com>
2026-08-25 20:20:23 +0530

    Fix script bundling and entry point

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './App.css'
-import './index.html'
-import './script.js'
-import './style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
